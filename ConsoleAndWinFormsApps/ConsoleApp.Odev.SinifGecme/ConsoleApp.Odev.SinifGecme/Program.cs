cf5fd4ed1155cf77f8e82475dad1398a1acc96ca
Mehmet Görer <67355699+mehmetgorer@users.noreply.github.com>
2025-03-11 19:14:56 +0300

    Add files via upload

---
+++ ConsoleAndWinFormsApps/ConsoleApp.Odev.SinifGecme/ConsoleApp.Odev.SinifGecme/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleApp.Odev.SinifGecme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////// Girilen 2 sınav notunun ortalamasını hesaplayıp geçme derecesini gösteren uygulamayı yazınız.( AND-OR kullanılmayacak)
+            ////// 0-49 Kaldı
+            ////50-59 CC
+            ////60-69 CB
+            ////70-79 BB
+            ////80-89 BA
+            ////90-100 AA
+            ///
+
+
+            Console.WriteLine("1. sınav notunu girin: ");
+            int s1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. sınav notunu girin: ");
+            int s2 = int.Parse(Console.ReadLine());
+
+            double ortalama = (s1 + s2) / 2.0;
+
+            string derece = "Kaldı";
+
+            if (ortalama >= 90)
+            {
+                derece = "AA";
+            }
+            else if (ortalama >= 80)
+            {
+                derece = "BA";
+            }
+            else if (ortalama >= 70)
+            {
+                derece = "BB";
+            }
+            else if (ortalama >= 60)
+            {
+                derece = "CB";
+            }
+            else if (ortalama >= 50)
+            {
+                derece = "CC";
+            }
+
+            if (derece == "Kaldı")
+            {
+                Console.WriteLine("Ortalama: " + ortalama);
+                Console.WriteLine("Geçme Durumu: Kaldı");
+            }
+            else
+            {
+                Console.WriteLine("Ortalama: " + ortalama);
+                Console.WriteLine("Geçme Durumu: Geçti");
+                Console.WriteLine("Geçme Derecesi: " + derece);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAndWinFormsApps/ConsoleApp.YasOrtalama/ConsoleApp.Odev1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp.Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kizSayisi = 0;
+            int kizYasToplami = 0;
+            int erkekSayisi = 0;
+            int erkekYasToplami = 0;
+
+            Console.WriteLine("Cinsiyet (kız/erkek), çıkmak için 'x' tuşuna basın");
+            string cinsiyet = Console.ReadLine().ToLower();
+
+            if (cinsiyet != "x")
+            {
+                Console.WriteLine("Yaş: ");
+                int yas = int.Parse(Console.ReadLine());
+
+                if (cinsiyet == "kız")
+                {
+                    kizSayisi = kizSayisi + 1;
+                    kizYasToplami += yas;
+                }
+                else if (cinsiyet == "erkek")
+                {
+                    erkekSayisi = erkekSayisi + 1;
+                    erkekYasToplami += yas;
+                }
+
+            }
+
+            double kizYasOrtalama = kizSayisi > 0 ? kizYasToplami / (double)kizSayisi : 0;
+            double erkekYasOrtalamasi = erkekSayisi > 0 ? erkekYasToplami / (double)erkekSayisi : 0;
+
+            Console.WriteLine("Kızların sayısı: " + kizSayisi);
+            Console.WriteLine("Kızların yaş ortalaması: " + kizYasOrtalama);
+            Console.WriteLine("Erkeklerin sayısı: " + erkekSayisi);
+            Console.WriteLine("Erkeklerin yaş ortalaması: " + erkekYasOrtalamasi);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
